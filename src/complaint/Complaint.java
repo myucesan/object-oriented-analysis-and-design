df3d23673771bca77552696280c42be952033a65
Robin de Jong <robin.de.jong9@hva.nl>
2018-10-16 09:19:38 +0200

    Added all (most) properties for the complaint class

--- src/complaint/Complaint.java
+++ src/complaint/Complaint.java
@@ -1,4 +1,26 @@
 package complaint;
 
+import common.Image;
+import user.Property;
+import user.User;
+
+import java.util.List;
+
 public class Complaint {
+    private int id;
+    private String name;
+    private String description;
+    private Property property;
+    private Category category;
+    private Status status;
+    private List<Image> imageList;
+    private User customer;
+    private User assignee;
+    private List<History> history;
+
+    public enum Status {NEW, OPEN, ASSIGNED, CLOSED}
+
+    public enum Category {NEW, OPEN, ASSIGNED, CLOSED}
+
+
 }
